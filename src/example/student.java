4cd73a158dceef1a0bcf9336dcecb4ca8533285f
XS170411104434 <1746718216@qq.com>
2019-05-24 23:08:12 +0800

    上传

---
+++ src/example/student.java
@@ -0,0 +1,10 @@
+package example;
+
+public class student {
+
+	public static void main(String[] args) {
+		System.out.println("I am a student!");
+
+	}
+
+}
---
+++ src/example/teacher.java
@@ -0,0 +1,10 @@
+package example;
+
+public class teacher {
+
+	public static void main(String[] args) {
+		System.out.println("I am a teacher!");
+
+	}
+
+}
